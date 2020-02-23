910bf01bb184bb28fc018ebd17add3aed79da190
Azzam Alsharafi <azzamalsharafi@gmail.com>
2020-02-23 20:48:26 +0300

    Add few functions to OccurrenceDao interface.
    Add few functions to OccurrenceDao interface.

--- app/src/main/java/com/azzam/timey/data/dao/OccurrenceDao.kt
+++ app/src/main/java/com/azzam/timey/data/dao/OccurrenceDao.kt
@@ -6,8 +6,11 @@ import com.azzam.timey.data.entity.Occurrence
 
 @Dao
 interface OccurrenceDao {
-    @Query("SELECT * FROM occurrences_table")
-    fun getAll(): LiveData<List<Occurrence>>
+    @Query("SELECT * FROM occurrences_table WHERE start_date_time <= :rangeEnd AND end_date_time >= :rangeStart")
+    fun getAllInTimeRange(rangeStart: Long, rangeEnd: Long): LiveData<List<Occurrence>>
+
+    @Query("SELECT * FROM occurrences_table WHERE parent_id == :id AND parent_type == :type")
+    fun getAllOfParent(id: Int, type: Int): LiveData<List<Occurrence>>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(occurrences: List<Occurrence>): List<Long>
