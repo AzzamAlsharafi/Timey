1e47243877415093ce1abed9e484f833b80411a1
Azzam Alsharafi <azzamalsharafi@gmail.com>
2020-02-23 17:36:17 +0300

    Add parentType variable in Occurrence class.
    Add parentType variable in Occurrence class.

--- app/src/main/java/com/azzam/timey/data/entity/Occurrence.kt
+++ app/src/main/java/com/azzam/timey/data/entity/Occurrence.kt
@@ -10,6 +10,9 @@ data class Occurrence(
     @ColumnInfo(name = "parent_id")
     var parentId: Int,
 
+    @ColumnInfo(name = "parent_type")
+    var parentType: Int,
+
     @ColumnInfo(name = "start_date_time")
     var startDateTime: Date,
 
@@ -18,4 +21,10 @@ data class Occurrence(
 
     @PrimaryKey(autoGenerate = true)
     val id: Long = 0
-)
+) {
+    companion object {
+        val PARENT_EVENT: Int = 0
+        val PARENT_TASK: Int = 1
+        val PARENT_HABIT: Int = 2
+    }
+}
