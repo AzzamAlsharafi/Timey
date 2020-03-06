f992464cfd7f530a8b63e262e6d2ceb965b16ec0
Azzam Alsharafi <azzamalsharafi@gmail.com>
2020-03-06 15:10:53 +0300

    Create Task's occurrences-generating functions.
    Create Task's occurrences-generating functions.

--- app/src/main/java/com/azzam/timey/data/OccurrencesGenerator.kt
+++ app/src/main/java/com/azzam/timey/data/OccurrencesGenerator.kt
@@ -3,6 +3,7 @@ package com.azzam.timey.data
 import com.azzam.timey.data.entity.Event
 import com.azzam.timey.data.entity.Occurrence
 import com.azzam.timey.data.entity.Repeating
+import com.azzam.timey.data.entity.Task
 import org.threeten.bp.Duration
 import org.threeten.bp.LocalDate
 import org.threeten.bp.OffsetDateTime
@@ -31,13 +32,13 @@ object OccurrencesGenerator {
 
     fun generateEventOccurrences(event: Event): List<Occurrence> {
         return when (event.repeating.endType) {
-            Repeating.DATE -> generateWithEndDate(event)
-            Repeating.OCCURRENCES -> generateWithOccurrencesCount(event)
-            else -> generateWithOccurrencesCount(event) // Repeating.NEVER
+            Repeating.DATE -> eventGenerateWithEndDate(event)
+            Repeating.OCCURRENCES -> eventGenerateWithOccurrencesCount(event)
+            else -> eventGenerateWithOccurrencesCount(event) // Repeating.NEVER
         }
     }
 
-    private fun generateWithOccurrencesCount(event: Event): List<Occurrence> {
+    private fun eventGenerateWithOccurrencesCount(event: Event): List<Occurrence> {
         val occurrences: MutableList<Occurrence> = mutableListOf()
 
         val r = event.repeating
@@ -87,7 +88,7 @@ object OccurrencesGenerator {
         return occurrences.toList()
     }
 
-    private fun generateWithEndDate(event: Event): List<Occurrence> {
+    private fun eventGenerateWithEndDate(event: Event): List<Occurrence> {
         val occurrences: MutableList<Occurrence> = mutableListOf()
 
         val r = event.repeating
@@ -121,6 +122,101 @@ object OccurrencesGenerator {
                         occStart,
                         occEnd,
                         event.timezone
+                    )
+                    occurrences.add(occurrence)
+                }
+                matchingDaysCounts++
+            }
+            date = date.plusDays(1)
+        }
+
+        return occurrences.toList()
+    }
+
+    fun generateTaskOccurrences(task: Task): List<Occurrence> {
+        return when (task.repeating.endType) {
+            Repeating.DATE -> taskGenerateWithEndDate(task)
+            Repeating.OCCURRENCES -> taskGenerateWithOccurrencesCount(task)
+            else ->  taskGenerateWithOccurrencesCount(task)// Repeating.NEVER
+        }
+    }
+
+    private fun taskGenerateWithOccurrencesCount(task: Task): List<Occurrence> {
+        val occurrences: MutableList<Occurrence> = mutableListOf()
+
+        val r = task.repeating
+
+        val count = if (r.endExtra.isNotEmpty()) {
+            r.endExtra.toInt()
+        } else {
+            Occurrence.MAX_OCCURRENCES_COUNT
+        }
+
+        val start = task.dateTime.atZoneSameInstant(task.timezone)
+
+        var date = start.toLocalDate()
+
+        var matchingDaysCounts = 0
+
+        val check = when (r.repeatingType) {
+            Repeating.WEEKLY -> checkDayOfWeek
+            Repeating.MONTHLY_BY_DAY -> checkDayOfMonth
+            Repeating.MONTHLY_BY_DAY_OF_WEEK -> checkDayOfWeekOfMonth
+            Repeating.YEARLY -> checkDayOfYear
+            else -> checkDaily // Repeating.DAILY
+        }
+
+        while ((matchingDaysCounts / r.repeatingValue) < count) {
+            if (check(date, start.toLocalDate())) {
+                if (matchingDaysCounts % r.repeatingValue == 0) {
+                    val occStart: OffsetDateTime = start.with(date).toOffsetDateTime()
+                    val occurrence = Occurrence(
+                        task.id,
+                        Occurrence.PARENT_TASK,
+                        occStart,
+                        occStart,
+                        task.timezone
+                    )
+                    occurrences.add(occurrence)
+                }
+                matchingDaysCounts++
+            }
+            date = date.plusDays(1)
+        }
+
+        return occurrences.toList()
+    }
+
+    private fun taskGenerateWithEndDate(task: Task): List<Occurrence> {
+        val occurrences: MutableList<Occurrence> = mutableListOf()
+
+        val r = task.repeating
+
+        val start = task.dateTime.atZoneSameInstant(task.timezone)
+
+        var date = start.toLocalDate()
+        val rEnd = LocalDate.parse(r.endExtra).plusDays(1)
+
+        var matchingDaysCounts = 0
+
+        val check = when (r.repeatingType) {
+            Repeating.WEEKLY -> checkDayOfWeek
+            Repeating.MONTHLY_BY_DAY -> checkDayOfMonth
+            Repeating.MONTHLY_BY_DAY_OF_WEEK -> checkDayOfWeekOfMonth
+            Repeating.YEARLY -> checkDayOfYear
+            else -> checkDaily // Repeating.DAILY
+        }
+
+        while (date.isBefore(rEnd)) {
+            if (check(date, start.toLocalDate())) {
+                if (matchingDaysCounts % r.repeatingValue == 0) {
+                    val occStart: OffsetDateTime = start.with(date).toOffsetDateTime()
+                    val occurrence = Occurrence(
+                        task.id,
+                        Occurrence.PARENT_TASK,
+                        occStart,
+                        occStart,
+                        task.timezone
                     )
                     occurrences.add(occurrence)
                 }
